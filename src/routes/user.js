b14fa76d6c3baaa39f3627f8b2035eb37f3bc07a
yamasya54 <yavuzamasya1@gmail.com>
2023-12-30 21:00:22 +0300

    password uptade

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,6 +6,9 @@ const user = require("../controllers/user");
 
 router.route("/").get(user.list).post(user.create);
 
+router.route("/updatePassword").post(user.uptadePassword);
+
 router.route("/:id").get(user.read).put(user.update).delete(user.delete);
+
 
 module.exports = router;
