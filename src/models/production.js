c0bb09251aa47e6ab0962c6cf0aa2b399c7621e2
Ekrem <avekremyilmazturk@gmail.com>
2024-01-04 13:55:59 +0300

    up

--- src/models/production.js
+++ src/models/production.js
@@ -9,18 +9,31 @@ const roles = {
 
 const Production = sequelize.define("Production", {
   status: {
-
+    type: DataTypes.ENUM(Object.keys(roles)),
+    allowNull: false,
+    defaultValue:1
   },
-  creatorId: {
-
+  creatorId: {                      // referans alacağımız yer var mı?
+    type: DataTypes.INTEGER,
+    allowNull: false
   },
   updaterId: {
-
+    type: DataTypes.INTEGER,
+    allowNull: false
   },
   vehicleId: {
-
-  },
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+        model: 'Vehicle', 
+        key: 'id',
+  }},
   saleId: {
-    
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+        model: 'Sales', 
+        key: 'id',
+  }
   },
 });
