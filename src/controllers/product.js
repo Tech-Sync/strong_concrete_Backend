224579b55fc3c2b2b394dea95e221a03d2854458
yamasya54 <yavuzamasya1@gmail.com>
2024-01-06 21:43:29 +0300

    prodoct

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,43 +1,84 @@
 "use strict";
 
-const {Product} = require('../models/product')
+const { Product } = require("../models/product");
 
 module.exports = {
-    list: async (req, res) => {
-        const data = await Product.findAndCountAll({paranoid: false})
+  list: async (req, res) => {
+    const data = await Product.findAndCountAll({ paranoid: false });
 
-        res.status(200).send(data)
-    },
+    res.status(200).send(data);
+  },
 
-    create: async (req, res) => {
-        req.body.creatorId = req.user.id
-        const data = await Product.create(req.body)
+  
+   create: async (req, res) => {
+  try {
+    const { materials } = req.body;
 
-        res.status(200).send(data)
-    },
+    // Gerekli anahtarların (STONE, SAND, CEMENT) obje içinde olup olmadığını kontrol et
+    const requiredKeys = ['STONE', 'SAND', 'CEMENT'];
+    const hasRequiredKeys = requiredKeys.every(key => materials.hasOwnProperty(key));
 
-    read: async (req, res) => {
-        const data = await Product.findByPk(req.params.id)
-        if (!data) throw new Error("Product not found!")
+    if (!hasRequiredKeys) {
+      return res.status(400).send({ error: 'Malzeme bilgilerinde eksik veya geçersiz anahtarlar.' });
+    }
 
-        res.status(200).send(data)
-    },
+    // Diğer kontrolleri buraya ekleyebilirsiniz.
 
-    update: async (req, res) => {
-        const isUpdated = await Product.update(req.body, {
-            where: {id: req.params.id}, individualHooks: true,
-        });
+    // Geçerli veri ise creatorId ekleyip kaydet
+    req.body.creatorId = req.user.id;
+    const data = await Product.create(req.body);
 
-        res.status(202).send({
-            isUpdated: Boolean(isUpdated[0]), data: await Product.findByPk(req.params.id)
-        })
-    },
+    res.status(200).send(data);
+  } catch (error) {
+    console.error('Ürün oluşturulurken bir hata oluştu:', error);
+    res.status(500).send({ error: 'Bir hata oluştu, lütfen tekrar deneyin.' });
+  }
+},
 
-    delete: async (req, res) => {
-        const isDeleted = await Product.destroy({where: {id: req.params.id}});
+  
 
-        res.status(isDeleted ? 204 : 404).send({
-            error: !Boolean(isDeleted),
-            message: isDeleted ? "Product deleted succesfully" : "Product not found or something went wrong."
-        })
-    }}
+  read: async (req, res) => {
+    const data = await Product.findByPk(req.params.id);
+    if (!data) throw new Error("Product not found!");
+
+    res.status(200).send(data);
+  },
+
+  update: async (req, res) => {
+    req.body.updaterId = req.user.id;
+    const isUpdated = await Product.update(req.body, {
+      where: { id: req.params.id },
+      individualHooks: true,
+    });
+
+    res.status(202).send({
+      isUpdated: Boolean(isUpdated[0]),
+      data: await Product.findByPk(req.params.id),
+    });
+  },
+
+  delete: async (req, res) => {
+    const isDeleted = await Product.destroy({ where: { id: req.params.id } });
+
+    res.status(isDeleted ? 204 : 404).send({
+      error: !Boolean(isDeleted),
+      message: isDeleted
+        ? "Product deleted succesfully"
+        : "Product not found or something went wrong.",
+    });
+  },
+
+  restore: async (req, res) => {
+    const product = await Product.findByPk(req.params.id, { paranoid: false });
+    if (!product) throw new Error("Product not Found.");
+    product.updaterId = req.user.id;
+    const isRestored = await product.restore();
+
+    res.status(200).send({
+      error: !Boolean(isRestored),
+      message: isRestored
+        ? "Product restored successfuly."
+        : "Product not found or something went wrong.",
+    });
+  },
+};
