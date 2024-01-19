47609e0cb23c256d932fb602c3a49cf47fdf06c3
yamasya54 <yavuzamasya1@gmail.com>
2024-01-19 16:08:00 +0300

    sc-59

--- src/controllers/saleAccount.js
+++ src/controllers/saleAccount.js
@@ -6,6 +6,16 @@ module.exports = {
   list: async (req, res) => {
     /* 
         #swagger.tags = ['Sale Account']
+        #swagger.summary = ' Sale Account List'
+        #swagger.description = '
+        <b>-</b> You can send query with endpoint for search[], sort[], page and limit. <br>
+        
+                <ul> Examples:
+                    <li><b>SEARCHING: URL?search[FirmId]=3&search[quantity]=20</b></li>
+                    <li><b>SORTING: URL?sort[quantity]=desc&sort[totalPrice]=asc</b></li>
+                    <li><b>PAGINATION: URL?page=1&limit=10&offset=10</b></li>
+                    <li><b>DATE FILTER: URL?startDate=2023-07-13&endDate=2023-10-01  The date must be in year-month-day format</b></li>
+                </ul>'
     */
     const data = await SaleAccount.findAndCountAll({});
 
@@ -15,6 +25,15 @@ module.exports = {
   create: async (req, res) => {
     /* 
         #swagger.tags = ['Sale Account']
+        #swagger.summary = 'Sale Account Create'
+        #swagger.description = '
+          <b>-</b> SaleAccount will be created automatically after the sale is made. <br>
+          <b>-</b> Send access token in header.'
+        #swagger.parameters['body'] = {
+        in: 'body',
+        required: true,
+        
+      }
     */
     req.body.creatorId = req.user.id;
 
@@ -26,6 +45,9 @@ module.exports = {
   read: async (req, res) => {
     /* 
         #swagger.tags = ['Sale Account']
+         #swagger.summary = 'Read Sale Account with id'
+        #swagger.description = '
+       <b>-</b> Send access token in header. '
     */
     const data = await SaleAccount.findByPk(req.params.id);
     if (!data) throw new Error("SaleAccount not found !");
@@ -35,6 +57,20 @@ module.exports = {
   update: async (req, res) => {
     /* 
         #swagger.tags = ['Sale Account']
+        #swagger.summary = 'Update Sale Account with id'
+        #swagger.description = `<b>-</b> Send access token in header.`
+        #swagger.parameters['body'] = {
+          in: 'body',
+          description: '
+            <ul> 
+              <li>Send the object includes attributes that should be updated.</li>
+              
+            </ul> ',
+          required: true,
+          schema: {
+            FirmId:'test1'
+          }
+        } 
     */
     req.body.updaterId = req.user.id;
     const isUpdated = await SaleAccount.update(req.body, {
@@ -51,6 +87,8 @@ module.exports = {
   delete: async (req, res) => {
     /* 
         #swagger.tags = ['Sale Account']
+        #swagger.summary = 'Delete Sale Account with id'
+        #swagger.description = '<b>-</b> Send access token in header.'
     */
     const saleAccount = await SaleAccount.findByPk(req.params.id);
     saleAccount.updaterId = req.user.id;
@@ -67,6 +105,8 @@ module.exports = {
   restore: async (req, res) => {
     /* 
         #swagger.tags = ['Sale Account']
+         #swagger.summary = 'Restore deleted Sale Account with id'
+        #swagger.description = `<b>-</b> Send access token in header.`
     */
     const saleAccount = await SaleAccount.findByPk(req.params.id, {
       paranoid: false,
