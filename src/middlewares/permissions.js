765903368ac51fae3bd6c78f22186a708c231610
Ali <alidrl26@gmail.com>
2024-01-20 19:03:03 +0200

    SC-53 user and auth permissions

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -1,20 +1,49 @@
 "use strict";
+
+function getUserInfo(req) {
+  let user = req.user;
+  let isActive = req.user?.isActive;
+  let userId = req.user?.id;
+  let userRole = req.user?.role;
+
+  return { user, isActive, userId, userRole };
+}
+
 module.exports = {
   isLogin: (req, res, next) => {
     res.errorStatusCode = 403;
-    if (req.user && req.user.isActive) next();
+    const { user, isActive } = getUserInfo(req);
+    if (user && isActive) next();
     else throw new Error("NoPermission: You must login.");
   },
 
   isAdmin: (req, res, next) => {
     res.errorStatusCode = 403;
-    if (req.user && req.user.isActive && req.user.role === 5) next();
-    else throw new Error("NoPermission: You must login and to be Admin.");
+    const { userRole } = getUserInfo(req);
+
+    if (userRole === 5) next();
+    else throw new Error("NoPermission: You must login and be Admin.");
+  },
+
+  isItself: (req, res, next) => {
+    res.errorStatusCode = 403;
+    const { userRole, userId } = getUserInfo(req);
+
+    if (userRole === 5 || req.params.id == userId)
+      next(); // string(9) == number(9) -> true
+    else
+      throw new Error(
+        "NoPermission: You are not authorized for this operation."
+      );
   },
 
   hasFirmAccess: (req, res, next) => {
     res.errorStatusCode = 403;
-    if ((req.user && req.user.isActive) && [5, 3, 4].includes(req.user.role) )  next();
-      else throw new Error("NoPermission: You must login and have sufficient role for firm operations.");
+    if (req.user && req.user.isActive && [5, 3, 4].includes(req.user.role))
+      next();
+    else
+      throw new Error(
+        "NoPermission: You must login and have sufficient role for firm operations."
+      );
   },
 };
