127aba3916f3fc893d8465e70115bd7d95a8d811
Ali <alidrl26@gmail.com>
2024-02-01 23:56:36 +0200

    SC-91 auth response updated

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,12 +2,12 @@
 
 const router = require("express").Router();
 const auth = require("../controllers/auth");
-const permissions = require('../middlewares/permissions')
+const permissions = require("../middlewares/permissions");
 
 router.post("/login", auth.login);
 router.get("/verify-email", auth.verifyEmail);
 router.post("/register", auth.register); // only admin can create a user
-router.post("/refresh",permissions.isLogin, auth.refresh);
+router.post("/refresh", auth.refresh);
 router.get("/logout", auth.logout);
 
 module.exports = router;
