6bd9c3ce09d9b84255be983a544562a5b7476030
Ali <alidrl26@gmail.com>
2024-02-27 14:34:37 +0200

    db config file updated

--- src/configs/dbConnection.js
+++ src/configs/dbConnection.js
@@ -4,6 +4,7 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_HOST = process.env.DB_HOST;
+const HOST = process.env.HOST;
 const DB = process.env.DB;
 
 /* const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
