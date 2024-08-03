7362a0f48c1905a348408dd9fde5d71ac356d45a
AliDurul <alidrl26@gmail.com>
2024-08-03 18:26:52 +0200

    chore: Refactor dashboard controller to include statistics endpoint

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -6,91 +6,167 @@ const Purchase = require("../models/purchase");
 const Sale = require("../models/sale");
 const sale = require("./sale");
 const purchase = require("./purchase");
-const { Op } = require("sequelize");
+const { Op, fn, col } = require("sequelize");
 const moment = require("moment");
+const Product = require("../models/product");
 
 
-module.exports = {
-    list: async (req, res) => {
-        /* 
-            #swagger.tags = ['Firm']
-            #swagger.summary = 'List All Firms'
-            #swagger.description = `You can send query with endpoint for search[], sort[], page and limit.
-              <ul> Examples:
-                  <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
-                  <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
-                  <li>URL/?<b>page=2&limit=1</b></li>
-              </ul>
-            `
-            #swagger.parameters['showDeleted'] = {
-            in: 'query',
-            type: 'boolean',
-            description:'Send true to show deleted data as well, default value is false'
-          }
-        */
-        const endOfLastWeek = moment().subtract(1, 'weeks').endOf('isoWeek').toDate()
+async function getStatsData() {
+    const endOfLastWeek = moment().subtract(1, 'weeks').endOf('isoWeek').toDate();
 
+    // Helper function to calculate total price
+    const calculateTotalPrice = (items) => items.reduce((acc, item) => acc + item.totalPrice, 0);
 
-        /* SALE */
-        const allSale = await Sale.findAndCountAll({ where: { status: 2, deletedAt: null } });
-
-        const totalSaleAmount = allSale.rows.reduce((acc, sale) => {
-            return acc + sale.totalPrice;
-        }, 0)
-
-
-        const salesUpToLastWeek = await Sale.findAll({
+    // SALE
+    const [allSale, salesUpToLastWeek] = await Promise.all([
+        Sale.findAndCountAll({ where: { status: 2, deletedAt: null } }),
+        Sale.findAll({
             where: {
                 status: 2,
                 deletedAt: null,
                 orderDate: { [Op.lte]: endOfLastWeek }
             }
         })
+    ]);
 
-        const totalSaleAmountUpToLastWeek = salesUpToLastWeek.reduce((acc, sale) => {
-            return acc + sale.totalPrice;
-        }, 0);
+    const totalSale = calculateTotalPrice(allSale.rows);
+    const totalSaleLastWeek = calculateTotalPrice(salesUpToLastWeek);
+    const salePerChange = totalSaleLastWeek === 0
+        ? (totalSale === 0 ? 0 : 100)
+        : ((totalSale - totalSaleLastWeek) / totalSaleLastWeek) * 100;
 
-
-        /* PURCHASE */
-        const allPurchase = await Purchase.findAndCountAll({ whewe: { deletedAt: null } });
-
-        const totalPurchaseAmount = allPurchase.rows.reduce((acc, purchase) => {
-            return acc + purchase.totalPrice;
-        }, 0)
-
-        const purchasesUpToLastWeek = await Purchase.findAll({
+    // PURCHASE
+    const [allPurchase, purchasesUpToLastWeek] = await Promise.all([
+        Purchase.findAndCountAll({ where: { deletedAt: null } }),
+        Purchase.findAll({
             where: {
                 deletedAt: null,
                 createdAt: { [Op.lte]: endOfLastWeek }
             }
         })
+    ]);
 
-        const totalPurchasesAmountUpToLastWeek = purchasesUpToLastWeek.reduce((acc, purchase) => {
-            return acc + purchase.totalPrice;
-        }, 0);
+    const totalPurchase = calculateTotalPrice(allPurchase.rows);
+    const totalPurchaseLastWeek = calculateTotalPrice(purchasesUpToLastWeek);
+    const purchasePercChange = totalPurchaseLastWeek === 0
+        ? (totalPurchase === 0 ? 0 : 100)
+        : ((totalPurchase - totalPurchaseLastWeek) / totalPurchaseLastWeek) * 100;
+
+    // PROFIT
+    const totalProfit = totalSale - totalPurchase;
+    const totalProfitLastWeek = totalSaleLastWeek - totalPurchaseLastWeek;
+    const profitPercentageChange = totalProfitLastWeek === 0
+        ? (totalProfit === 0 ? 0 : 100)
+        : ((totalProfit - totalProfitLastWeek) / totalProfitLastWeek) * 100;
+
+    return {
+        sale: {
+            totalSale,
+            totalSaleLastWeek,
+            salePerChange,
+            SaleCount: allSale.count
+        },
+        purchase: {
+            totalPurchase,
+            totalPurchaseLastWeek,
+            purchasePercChange,
+            purchaseCount: allPurchase.count
+        },
+        profit: {
+            totalProfit,
+            totalProfitLastWeek,
+            profitPercentageChange
+        }
+    };
+}
+
+
+async function getRevenueChartData() {
+    const startOfYear = moment().startOf('year').toDate();
+    const endOfYear = moment().endOf('year').toDate();
+    const monthlySales = Array(12).fill(0);
+    const monthlyPurchases = Array(12).fill(0);
+
+    const sales = await Sale.findAll({
+        where: {
+            status: 2,
+            deletedAt: null,
+            orderDate: {
+                [Op.gte]: startOfYear,
+                [Op.lte]: endOfYear
+            }
+        }
+    });
+
+    const purchases = await Purchase.findAll({
+        where: {
+            deletedAt: null,
+            createdAt: {
+                [Op.gte]: startOfYear,
+                [Op.lte]: endOfYear
+            }
+        }
+    });
+
+    sales.forEach(sale => {
+        const month = moment(sale.orderDate).month();
+        monthlySales[month] += sale.totalPrice;
+    });
+
+    purchases.forEach(purchase => {
+        const month = moment(purchase.createdAt).month();
+        monthlyPurchases[month] += purchase.totalPrice;
+    });
+
+    return { monthlySales, monthlyPurchases };
+}
+
+async function getSalesByCategoryData() {
+
+    const sales = await Sale.findAll({
+        where: {
+            status: 2,
+            deletedAt: null
+        },
+        attributes: [
+            'ProductId',
+            [fn('SUM', col('quantity')), 'totalQuantity']
+        ],
+        include: [
+            {
+                model: Product,
+                attributes: ['name']
+            }
+        ],
+        group: ['ProductId', 'Product.id'],
+    });
+
+    const productName = sales.map(sale => sale.Product.name)
+    const productQuantity = sales.map(sale => sale.get('totalQuantity'))
+
+    return { productName, productQuantity, }
+
+}
+
+
+module.exports = {
+    list: async (req, res) => {
+
+
+        const [stats, revenueChart, salesByCategory] = await Promise.all([
+            getStatsData(),
+            getRevenueChartData(),
+            getSalesByCategoryData()
+        ]);
 
 
 
         res.status(200).send({
             error: false,
             statistics: {
-                stats: {
-                    sale: {
-                        // allSale,
-                        totalSaleAmount,
-                        totalNumberSale: allSale.count,
-                        totalSaleAmountUpToLastWeek
-                    },
-                    purchase: {
-                        // allPurchase,
-                        totalPurchaseAmount,
-                        totalNumberPurchase: allPurchase.count,
-                        totalPurchasesAmountUpToLastWeek
-                    },
-                    totalProfit: totalSaleAmount - totalPurchaseAmount
-                }
-                
+                stats,
+                revenueChart,
+                salesByCategory
             }
         });
     },
