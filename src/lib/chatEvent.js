cba85203b1ad616f7c81c77f56bcf1fc945f52e6
AliDurul <alidrl26@gmail.com>
2024-10-02 12:56:03 +0200

    Refactor socket.io integration: separate socket configuration into a dedicated module and add userConnected event

--- src/lib/chatEvent.js
+++ src/lib/chatEvent.js
@@ -1,42 +1,75 @@
 const io = require('../configs/socketConfig');
 
+const activeUsers = new Map();
+
 io.on("connection", (socket) => {
-    console.log('---socket connected---' + socket.id);
-  
-  
-    socket.on('joinRoom', (chatId) => {
-      socket.join(chatId)
-      console.log(`Socket ${socket.id} joined room ${chatId}`);
-  
-    })
-  
-    socket.on('leaveRoom', (chatId) => {
-      socket.leave(chatId)
-      console.log(`Socket ${socket.id} left room ${chatId}`);
-    })
-  
-    socket.on('sendMessage', (message) => {
-      console.log(`Message received from ${message.senderId} for chat ${message.chatId}`);
-  
-      socket.to(message.chatId).emit('receiveMessage', message)
-    })
-  
-    socket.on('typing', ({ chatId }) => {
-      console.log('someone is typing ın chat', chatId);
-      socket.to(chatId).emit('typing')
-    })
-  
-    socket.on('stopTyping', ({ chatId }) => {
-      console.log('someone is stpped in chat', chatId);
-  
-      socket.to(chatId).emit('stopTyping');
-    });
-  
-  
-    socket.on('disconnect', () => {
-      console.log('---socket disconnected---' + socket.id);
-    });
-  
+  console.log('---socket connected--- ' + socket.id);
+
+  socket.on('userConnected', (userId) => {
+    activeUsers.set(socket.id, { userId });
+    emitActiveUsers();
+  });
+
+  socket.on('joinRoom', (chatId) => {
+    socket.join(chatId)
+    console.log(`Socket ${socket.id} joined room ${chatId}`);
+  })
+
+  socket.on('leaveRoom', (chatId) => {
+    socket.leave(chatId)
+    console.log(`Socket ${socket.id} left room ${chatId}`);
+  })
+
+  socket.on('sendMessage', (message, receiverId) => {
+    console.log(`Message received from ${message.senderId} userId  for chat ${message.chatId} id`);
+    socket.to(message.chatId).emit('receiveMessage', message)
+
+    const recipientSocket = Array.from(activeUsers.entries()).find(([id, user]) => user.userId === receiverId);
+    // active
+    if (recipientSocket) {
+      const [recipientSocketId, recepientUser] = recipientSocket
+      const isRecipientInChat = io.sockets.sockets.get(recipientSocketId)?.rooms.has(message.chatId)
+      // not in chat
+      if (!isRecipientInChat) {
+        io.to(recipientSocketId).emit('receiveNotification', message)
+      }
+      // not active not in chat
+    } else {
+      sendNotification(receiverId, message)
+    }
+
+  })
+
+  socket.on('typing', ({ chatId }) => {
+    console.log('someone is typing ın chat', chatId);
+    socket.to(chatId).emit('typing')
+  })
+
+  socket.on('stopTyping', ({ chatId }) => {
+    console.log('someone is stpped in chat', chatId);
+    socket.to(chatId).emit('stopTyping');
+  });
+
+
+  socket.on('disconnect', () => {
+    console.log('---socket disconnected---' + socket.id);
+
+    activeUsers.delete(socket.id);
+    emitActiveUsers();
+  });
+
+  const emitActiveUsers = () => {
+    const users = Array.from(activeUsers.values());
+    io.emit('activeUsers', users);
+  };
+
+  const sendNotification = (userId, message) => {
+    //  push notification service or email service 
+    console.log(`Sending notification to user ${userId} about message: ${message.content}`);
+
+  };
+
+
 });
-  
+
 module.exports = io
